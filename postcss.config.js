3fe59dccb4ca1f6031e95068690a9a97fd46a628
Aleks Moraless <aleksmoraless@yandex.ru>
2026-01-02 01:08:49 +0300

    fix autoprfx

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
 // postcss.config.js
 export default {
   plugins: {
-    autoprefixer: {}
+    'postcss-preset-env': {
+      stage: 3,
+      features: {
+        'nesting-rules': true
+      }
+    },
+    autoprefixer: {
+      overrideBrowserslist: [
+        'last 2 versions',
+        '> 1%',
+        'not IE 11',
+        'not dead'
+      ],
+      grid: 'autoplace' // важно для CSS Grid
+    }
   }
 }
